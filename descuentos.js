b31276a0846560bdb33cb05d40baad929d3eea06
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-03-27 13:46:32 -0600

    Modificacion solucion 2 de 3

--- descuentos.js
+++ descuentos.js
@@ -16,11 +16,35 @@ function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    let descuento;
 
+    if (!coupons.includes(couponValue)) {
+        alert("El cupón " + couponValue + "no es válido");
+     } else if (couponValue === "carlos") {
+        descuento = 15;
+     } else if (couponValue === "mata") {
+        descuento = 30;
+     } else if (couponValue === "carlozmata") {
+        descuento = 25;
+     }
+
+    switch(couponValue) {
+      case coupons[0]: // "JuanDC_es_Batman"
+        descuento = 15;
+      break;
+      case coupons[1]: // "pero_no_le_digas_a_nadie"
+        descuento = 30;
+      break;
+      case coupons[2]: // "es_un_secreto"
+        descuento = 25;
+      break;
+    }
+   
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+  
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
-}
+  }
